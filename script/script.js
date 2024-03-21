de71c6d031bc3c45ebd6805e0759eee4339ea59c
Mo-cabdi <154708850+Mo-cabdi@users.noreply.github.com>
2024-03-21 11:14:09 +0100

    Added pages

--- script/script.js
+++ script/script.js
@@ -38,3 +38,57 @@ menuSide();
 
 
 
+// const filtersDropdown1 = document.getElementById('filters__dropdown1');
+// console.log(filtersDropdown1);
+
+// const filtersDropdown2 = document.getElementById('filters__dropdown2');
+// console.log(filtersDropdown2);
+
+// const filtersDropdownMenu = document.querySelector('.filters__dropdown');
+// console.log(filtersDropdownMenu);
+
+// filtersDropdown1.addEventListener('click', ()=>{
+//   console.log('Yo')
+//   filtersDropDown(filtersDropdown1);
+// })
+// filtersDropdown2.addEventListener('click', ()=>{
+//   console.log('Wts Up')
+//   filtersDropDown(filtersDropdown2);
+
+// })
+
+
+
+// function filtersDropDown(par) {
+//   if(filtersDropdownMenu.classList.contains('filters__dropdown')) {
+
+//     par.classList.toggle('FilterDropDown');
+//   }
+// }
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   // Get all dropdown toggles
+//   var dropdownToggles = document.querySelectorAll('.filters__dropdown');
+
+//   // Add event listener to each dropdown toggle
+//   dropdownToggles.forEach(function(dropdownToggle) {
+//     dropdownToggle.addEventListener('click', function(event) {
+//       event.stopPropagation(); // Prevent click event from bubbling up
+      
+//       // Toggle the visibility of the dropdown menu
+//       var dropdownMenu = this.querySelector('.filters__dropdown');
+//       dropdownMenu.classList.toggle('show');
+//     });
+//   });
+
+//   // Close dropdown menus when clicking outside
+//   document.addEventListener('click', function(event) {
+//     var dropdownMenus = document.querySelectorAll('.filters__dropdown');
+//     dropdownMenus.forEach(function(dropdownMenu) {
+//       if (!dropdownMenu.contains(event.target)) {
+//         dropdownMenu.classList.remove('show');
+//       }
+//     });
+//   });
+// });
